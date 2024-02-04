de278bb601dff060f5fc793e6d85bbab0dc978ec
M.Pashaei <majid.pashayi443@gmail.com>
2024-02-04 17:08:12 +0330

    PROBLEM IN FOOTER STYLE

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,15 @@
+import{UilInstagram,UilFaceBook,UilTelegram,UilWhatsApp} from '@iconscout/react-unicons'
+import css from '../styles/Footer.module.css'
 export default function Footer() {
-return (
-<div>
-Footer
-</div>
-)
+  return (
+    <div className={css.container}>
+      <span>تمامی حقوق این اثر متعلق به گروه سامان می باشد</span>
+      <div className={css.social}>
+        <UilFaceBook />
+        <UilTelegram />
+        <UilWhatsApp />
+        <UilInstagram />
+      </div>
+    </div>
+  );
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,20 +1,29 @@
-import css from '../styles/Header.module.css'
-import Image from "next/image"
-import logo from '../assets/logo.png'
-export default function Header(){
-return (
+import css from "../styles/Header.module.css";
+import Image from "next/image";
+import logo from "../assets/logo.png";
+import { UilShoppingBag } from "@iconscout/react-unicons";
+export default function Header() {
+  return (
     <div className={css.header}>
-        {/* {logo side} */}
-        <div className={css.logo}>
-           <Image src={logoo} alt="" height={50} width={50} />
+      {/* {logo side} */}
+      <div className={css.logo}>
+        <Image src={logo} alt="" height={50} width={50} />
+        <span>Naab</span>
+      </div>
+      {/* MENU SIDE */}
+      <ul className={css.menu}>
+        <li>صفحه اصلی</li>
+        <li>فروشگاه</li>
+        <li>تماس با ما</li>
+        <li>ارتباط با ما</li>
+      </ul>
+      {/* Right Side */}
+      <div className="css rightSide">
+        <div className={css.cart}>
+          <UilShoppingBag size={35} color="#2E2E2E" />
+          <div className={css.badge}>1</div>
         </div>
-        {/* MENU SIDE */}
-        
-        <ul className="css menu">
-            <li>صفحه اصلی</li>
-            <li></li>
-            <li></li>
-        </ul>
+      </div>
     </div>
-)
+  );
 }
